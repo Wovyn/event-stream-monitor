b3bae94cef8b5e3706e0ef5d8f92e9d3b4acb693
Rj Villanueva <rj.villanueva89@gmail.com>
2020-12-17 13:42:38 +0800

    initial dashboard update

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,10 +1,10 @@
 <?php namespace App\Controllers;
 
-class Home extends BaseController
+class Dashboard extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message', $this->data);
+		return view('dashboard', $this->data);
 	}
 
 	//--------------------------------------------------------------------
