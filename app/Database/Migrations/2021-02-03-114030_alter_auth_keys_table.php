94a64ae69af1f14c6604fdcd9d18f8ed18ea3ccf
Rj Villanueva <rj.villanueva89@gmail.com>
2021-02-03 11:43:21 +0800

    fix auth_keys table with CI4 timestamp fields

---
+++ app/Database/Migrations/2021-02-03-114030_alter_auth_keys_table.php
@@ -0,0 +1,29 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AlterAuthKeysTable extends Migration
+{
+    public function up()
+    {
+        $fields = [
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'after' => 'aws_secret'
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'after' => 'created_at'
+            ]
+        ];
+
+        $this->forge->addColumn('auth_keys', $fields);
+    }
+
+    //--------------------------------------------------------------------
+
+    public function down()
+    {
+        $this->forge->dropColumn('auth_keys', ['created_at', 'updated_at']);
+    }
+}
