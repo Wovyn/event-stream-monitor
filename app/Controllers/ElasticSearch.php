bc14bb59bc4fa103cb489b0ac9e47b9d9b12c170
Rj Villanueva <rj.villanueva89@gmail.com>
2021-03-30 16:54:45 +0800

    added initial Elasticsearch library, update elasticsearch controller with CreateElasticsearchDomain test

--- app/Controllers/ElasticSearch.php
+++ app/Controllers/ElasticSearch.php
@@ -4,9 +4,22 @@ namespace App\Controllers;
 class ElasticSearch extends BaseController
 {
 
+    protected $authKeysModel,
+        $elasticsearch,
+        $keys;
+
     public function __construct() {
         parent::__construct();
 
+        $this->authKeysModel = new \App\Models\AuthKeysModel();
+
+        $this->keys = $this->authKeysModel->where('user_id', $this->data['user']->id)->first();
+        if($this->keys) {
+            $this->elasticsearch = new \App\Libraries\Elasticsearch([
+                'access' => $this->keys->aws_access,
+                'secret' => $this->keys->aws_secret
+            ]);
+        }
     }
 
     public function index() {
@@ -22,6 +35,24 @@ class ElasticSearch extends BaseController
         );
 
         return view('elasticsearch/index', $this->data);
+    }
+
+    // manual test for elasticsearch
+    public function CreateElasticsearchDomain() {
+        $result = $this->elasticsearch->CreateElasticsearchDomain([
+            'DomainName' => 'esm-test-02',
+            // 'EBSOptions' => [
+            //     'EBSEnabled' => true,
+            //     'VolumeSize' => 10,
+            //     'VolumeType' => 'gp2'
+            // ]
+        ]);
+
+        echo '<pre>' , var_dump($result) , '</pre>';
+    }
+
+    public function test() {
+        echo $this->elasticsearch->version();
     }
 
 }
---
+++ app/Libraries/Elasticsearch.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Libraries;
+
+use Aws\ElasticsearchService\Exception\ElasticsearchServiceException;
+
+class Elasticsearch extends Aws {
+
+    protected $elasticsearch, $version = '2015-01-01';
+
+    public function __construct($args) {
+        parent::__construct($args);
+
+        $config = [
+            'version' => $this->version
+        ];
+
+        $this->elasticsearch = $this->aws->createElasticsearchService($config);
+    }
+
+    public function setRegion($region) {
+        $config = [
+            'version' => $this->version,
+            'region' => $region
+        ];
+
+        $this->elasticsearch = $this->aws->createElasticsearchService($config);
+    }
+
+    public function CreateElasticsearchDomain($args = []) {
+        $result['error'] = false;
+        try {
+            $result['response'] = $this->elasticsearch->createElasticsearchDomain($args);
+        } catch (ElasticsearchServiceException $e) {
+            $result['error'] = true;
+            $result['message'] = $e->getAwsErrorMessage();
+
+            log_message('debug', 'CreateElasticsearchDomain: ' . $e->getMessage());
+        }
+
+        return $result;
+    }
+
+    public function version() {
+        return $this->version;
+    }
+
+}
+
+?>
