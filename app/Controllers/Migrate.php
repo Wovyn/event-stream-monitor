5cdafae368b5dee841f3c0d93645b9f512c921de
Rj Villanueva <rj.villanueva89@gmail.com>
2021-04-15 17:14:36 +0800

    added migrate/check function

--- app/Controllers/Migrate.php
+++ app/Controllers/Migrate.php
@@ -3,12 +3,14 @@ namespace App\Controllers;
 
 class Migrate extends \CodeIgniter\Controller {
 
-    protected $migrate;
-    protected $seeder;
+    protected $migrate,
+        $seeder,
+        $db;
 
     public function __construct()  {
         $this->migrate = \Config\Services::migrations();
         $this->seeder = \Config\Database::seeder();
+        $this->db = \Config\Database::connect();
     }
 
     public function index() {
@@ -37,5 +39,35 @@ class Migrate extends \CodeIgniter\Controller {
         } catch (\Exception $e) {
             echo $e;
         }
+    }
+
+    public function check() {
+        $this->migrate->setNamespace(null);
+
+        $result = [];
+        foreach ($this->migrate->findMigrations() as $migration) {
+            if($this->db->tableExists('migrations')) {
+                $check_migration = $this->db->table('migrations')->getWhere(['version' => $migration->version, 'class' => $migration->class]);
+                if(!count($check_migration->getResult())) {
+                    $result[] = [
+                        'version' => $migration->version,
+                        'name' => $migration->name,
+                        'success' => $this->do_migrate($migration->path, $migration->namespace)
+                    ];
+                }
+            } else {
+                $result[] = [
+                    'version' => $migration->version,
+                    'name' => $migration->name,
+                    'success' => $this->do_migrate($migration->path, $migration->namespace)
+                ];
+            }
+        }
+
+        echo '<pre>' , var_dump($result) , '</pre>';
+    }
+
+    public function do_migrate($path, $namespace) {
+        return $this->migrate->force($path, $namespace);
     }
 }
